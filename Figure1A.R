e67db0f115f2d9efc093f44c4fcbb796d216e6d3
SrenBlikdal <62331372+SrenBlikdal@users.noreply.github.com>
2023-01-25 23:12:42 +0100

    Update Figure1A.R

--- Figure1A.R
+++ Figure1A.R
@@ -1,10 +1,10 @@
-###Figure 1A
+###Figure 2A
 
 library(tidyverse)
 library(methylKit)
 library(GenomicRanges)
 
-load("methylKit_small.RData")
+load("{your_path}/MethylKit_19samples.RData")
 
 mean.meth_H<-reorganize(meth.filtered,sample.ids=c("D4","D6","D10","D12","D14","D15","D17","D18","D20"), treatment=c(0,0,0,0,0,0,0,0,0)) %>% percMethylation(.) %>% rowMeans(.)
 mean.meth_S<-reorganize(meth.filtered,sample.ids=c("D1","D2","D3","D5","D8","D9","D11","D13","D16","D19"), treatment=c(1,1,1,1,1,1,1,1,1,1)) %>% percMethylation(.) %>% rowMeans(.)
@@ -16,20 +16,20 @@ inputnames<-c(seq(-6000, -50, by = 50),seq(50, 4000, by= 50))
 
 headder<-c("chrom","chromStart","chromEnd","name", "score", "strand")
 
-H<-inputnames %>% map(function(x) read_tsv(paste0("~/Documents/Master_thesis/annotation/GCF_000233375.1_ICSASG_v2_genomic_79000_",x,"promoter.bed"), col_names = headder) %>% as(.,"GRanges") %>% subsetByOverlaps(mean.meth_H_GR,.) %>% as_tibble() %>% transmute(mean=mean.meth, pos=x))
-S<-inputnames %>% map(function(x) read_tsv(paste0("~/Documents/Master_thesis/annotation/GCF_000233375.1_ICSASG_v2_genomic_79000_",x,"promoter.bed"), col_names = headder) %>% as(.,"GRanges") %>% subsetByOverlaps(mean.meth_S_GR,.) %>% as_tibble() %>% transmute(mean=mean.meth, pos=x))
+#GCF_000233375.1_ICSASG_v2_genomic_79000_",x,"promoter.bed are created with script 
+H<-inputnames %>% map(function(x) read_tsv(paste0("~{your_path}/Figure1A/GCF_000233375.1_ICSASG_v2_genomic_79000_",x,"promoter.bed"), col_names = headder) %>% as(.,"GRanges") %>% subsetByOverlaps(mean.meth_H_GR,.) %>% as_tibble() %>% transmute(mean=mean.meth, pos=x))
+S<-inputnames %>% map(function(x) read_tsv(paste0("~{your_path}/Figure1A/GCF_000233375.1_ICSASG_v2_genomic_79000_",x,"promoter.bed"), col_names = headder) %>% as(.,"GRanges") %>% subsetByOverlaps(mean.meth_S_GR,.) %>% as_tibble() %>% transmute(mean=mean.meth, pos=x))
 
 all_H<-bind_rows(H) %>% group_by(pos) %>% summarise(., "m"=mean(mean), "sd" = sd(mean)) %>% mutate(., DS = "Healthy")
 all_S<-bind_rows(S) %>% group_by(pos) %>% summarise(., "m"=mean(mean), "sd" = sd(mean)) %>% mutate(., DS = "Sick")
 
 p<-bind_rows(all_H,all_S)
 
-#png("/home/projects/ku-cbd/data/HoloFish/morten_fish/Epigenome/Novogene_CrappyFish_20samples_24Sep20_Analyses/mean_meth.png", width = 1000, height = 400, units='mm', res = 300)
 ggplot(p, aes(x=as.numeric(pos),y=m, color=DS))+
   geom_line(size=2)+
   ylab("Mean methylation")+
   xlab("Position from TSS")+ 
   scale_color_manual(values=c("blue", "red"))+
+  ylim(c(0,100))+
   theme_bw(base_size=16)+
   guides(color=guide_legend("Disease status"))
-#dev.off()

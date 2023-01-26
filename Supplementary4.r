e456ba0e119e1b62580e2ac7bfb666543af98697
SrenBlikdal <62331372+SrenBlikdal@users.noreply.github.com>
2023-01-26 14:37:28 +0100

    Update Supplementary4.r

--- Supplementary4.r
+++ Supplementary4.r
@@ -2,6 +2,15 @@ library(tidyverse)
 
 load("~/Documents/Crappyfish/MethylKit_19samples.RData")
 
+### Run in R
+export.bed(mydmr, con = "~/Documents/Crappyfish/dmrs.bed")
+
+##This part I run in the terminal with input files from Reference_annotation 
+
+for feature in GCF_000233375.1_ICSASG_v2_genomic_250promoter.bed GCF_000233375.1_ICSASG_v2_genomic_750promoter.bed GCF_000233375.1_ICSASG_v2_genomic_5000promoter.bed GCF_000233375.1_ICSASG_v2_genomic_2500GB.bed  GCF_000233375.1_ICSASG_v2_genomic_2500GE.bed GCF_000233375.1_ICSASG_v2_genomic_GB.bed ; do  bedtools intersect -a dmrs.bed -b $feature -wb | awk -v i="$feature" '{print $16, i}'| tr ";" "\t" | awk '{print $2, $NF}'| cut -c 15- >> genes_X_DMR.txt ; done
+for feature in GCF_000233375.1_ICSASG_v2_genomic_250promoter.bed GCF_000233375.1_ICSASG_v2_genomic_750promoter.bed GCF_000233375.1_ICSASG_v2_genomic_5000promoter.bed GCF_000233375.1_ICSASG_v2_genomic_2500GB.bed  GCF_000233375.1_ICSASG_v2_genomic_2500GE.bed GCF_000233375.1_ICSASG_v2_genomic_GB.bed ; do  bedtools intersect -a dmrs.bed -b $feature -wb | awk -v i="$feature" '{print $10, i}' |cut -c 6- >> DMR_X_genes.txt ; done
+
+## Run in R
 ncbi<-read_tsv("~/Downloads/gene_result.txt") %>% dplyr::select(GeneID, Symbol, description, Aliases,Status)
 dmrs<-read_tsv("~/Documents/Crappyfish/DMR_X_genes.txt", col_names = c("seqnames", "start", "end",".X1","X2","feature"))
 genes<-read_delim("~/Documents/Crappyfish/genes_X_DMR.txt", delim=" ", col_names = c("gene", "feature"))
